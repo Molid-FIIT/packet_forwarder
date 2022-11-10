41822ddd3e06a8761cdc02d7d52ac4b943ef6d8d
Matej L <39196903+Sofurs@users.noreply.github.com>
2022-11-10 11:25:12 +0100

    initial log structure, different log types

--- lora_pkt_fwd/inc/molid.h
+++ lora_pkt_fwd/inc/molid.h
@@ -3,6 +3,18 @@
 
 #include "loragw_hal.h"
 
+enum molid_ftype = { JOIN_REQUEST = 0x0, JOIN_ACCEPT = 0x1, OTHER = 0xff }; 
+
+struct molid_log_s {
+    uint8_t mhdr_ftype     = 0;
+    int32_t fhdr_devaddr   = 0;
+    uint8_t fhdr_fctrl_ack = 0;
+    uint16_t join_devnonce = 0;
+};
+
+void create_log(struct lgw_pkt_rx_s* p, struct molid_log_s* m);
+void fhdr_data(struct lgw_pkt_rx_s* p, struct molid_log_s* m);
+void join_req_data(struct lgw_pkt_rx_s* p, struct molid_log_s* m);
 void molid_log(struct lgw_pkt_rx_s* p);
 
 #endif
--- lora_pkt_fwd/src/molid.c
+++ lora_pkt_fwd/src/molid.c
@@ -1,22 +1,35 @@
 #include "molid.h"
 
-void molid_log(struct lgw_pkt_rx_s* p) {
-    uint8_t mhdr_ftype = 0;
-    int32_t fhdr_devaddr = 0;
-    uint8_t fhdr_fctrl_ack = 0;
-    uint16_t join_devnonce = 0;
-    
+void create_log(struct lgw_pkt_rx_s* p, struct molid_log_s* m) {
     /* MHDR - FType */
-    mhdr_ftype = p->payload[0] & 224;
+    m.mhdr_ftype = p->payload[0] & 224;
+
+    if(m.mhdr_ftype == molid_ftype.JOIN_REQUEST) {
+        join_req_data(p, m);
+    }
+    else if(m.mhdr_ftype != molid_ftype.JOIN_ACCEPT) {
+        fhdr_data(p, m);
+    }
+}
+
+void fhdr_data(struct lgw_pkt_rx_s* p, struct molid_log_s* m) {
     /* FHDR - DevAddr */
-    fhdr_devaddr  = p->payload[1];
-    fhdr_devaddr |= p->payload[2] << 8;
-    fhdr_devaddr |= p->payload[3] << 16;
-    fhdr_devaddr |= p->payload[4] << 24;
+    m.fhdr_devaddr  = p->payload[1];
+    m.fhdr_devaddr |= p->payload[2] << 8;
+    m.fhdr_devaddr |= p->payload[3] << 16;
+    m.fhdr_devaddr |= p->payload[4] << 24;
     /* FHDR - ACK */
-    fhdr_fctrl_ack = p->payload[5] & 32;
+    m.fhdr_fctrl_ack = p->payload[5] & 32;
+}
+
+void join_req_data(struct lgw_pkt_rx_s* p, struct molid_log_s* m) {
     /* JRQ - DevNonce */
-    // TODO: check for join-request
-    join_devnonce  = p->payload[17];
-    join_devnonce |= p->payload[18] << 8;
+    m.join_devnonce  = p->payload[17];
+    m.join_devnonce |= p->payload[18] << 8;
+}
+
+void molid_log(struct lgw_pkt_rx_s* p) {
+   struct molid_log_s molid_log;
+
+   create_log(p, molid_log);
 }
