e903e19dd7c65e14614061be8aaae649d5d483bf
Matej L <39196903+Sofurs@users.noreply.github.com>
2022-11-13 21:44:53 +0100

    packet header updates

--- lora_pkt_fwd/inc/molid.h
+++ lora_pkt_fwd/inc/molid.h
@@ -6,15 +6,43 @@
 enum molid_ftype = { JOIN_REQUEST = 0x0, JOIN_ACCEPT = 0x1, OTHER = 0xff }; 
 
 struct molid_log_s {
-    uint8_t mhdr_ftype     = 0;
-    int32_t fhdr_devaddr   = 0;
-    uint8_t fhdr_fctrl_ack = 0;
-    uint16_t join_devnonce = 0;
+    /* MHDR */
+    uint8_t  mhdr_ftype     = 0; // is everywhere
+
+    /* FHDR */
+    uint32_t fhdr_devaddr   = 0;
+    uint8_t  fhdr_fctrl     = 0;
+    uint16_t fhdr_fcnt      = 0;
+
+    /* JOIN-REQ */
+    uint64_t join_eui       = 0;
+    uint64_t join_deveui    = 0;
+    uint16_t join_devnonce  = 0;
+
+    /* JOIN-ACCEPT */
+    uint32_t join_netid     = 0;
+
+    /* InfoDesc=0,1 or 2 */
+    uint32_t lat            = 0;
+    uint32_t lng            = 0;
+
+    /* InfoDesc=3 */
+    uint32_t join_netid     = 0;
+    uint32_t join_gwid      = 0;
+
+    /* somewhere over the rainbow */
+    uint32_t frequency      = 0;
+
+    /* Link ADR Commands */
+    uint8_t  data_rate      = 0;
+
+    uint32_t mic            = 0; // is everywhere
 };
 
 void create_log(struct lgw_pkt_rx_s* p, struct molid_log_s* m);
 void fhdr_data(struct lgw_pkt_rx_s* p, struct molid_log_s* m);
 void join_req_data(struct lgw_pkt_rx_s* p, struct molid_log_s* m);
+void join_acc_data(struct lgw_pkt_rx_s* p, struct molid_log_s* m);
 void molid_log(struct lgw_pkt_rx_s* p);
 
 #endif
--- lora_pkt_fwd/src/molid.c
+++ lora_pkt_fwd/src/molid.c
@@ -7,9 +7,16 @@ void create_log(struct lgw_pkt_rx_s* p, struct molid_log_s* m) {
     if(m.mhdr_ftype == molid_ftype.JOIN_REQUEST) {
         join_req_data(p, m);
     }
-    else if(m.mhdr_ftype != molid_ftype.JOIN_ACCEPT) {
+    else if (m.mhdr_ftype == molid_ftype.JOIN_ACCEPT) {
+        join_acc_data(p, m);
+    }
+    else {
         fhdr_data(p, m);
     }
+
+    // p->datarate // SF
+    // p->rssi
+    // p->snr
 }
 
 void fhdr_data(struct lgw_pkt_rx_s* p, struct molid_log_s* m) {
@@ -18,14 +25,34 @@ void fhdr_data(struct lgw_pkt_rx_s* p, struct molid_log_s* m) {
     m.fhdr_devaddr |= p->payload[2] << 8;
     m.fhdr_devaddr |= p->payload[3] << 16;
     m.fhdr_devaddr |= p->payload[4] << 24;
-    /* FHDR - ACK */
-    m.fhdr_fctrl_ack = p->payload[5] & 32;
+
+    /* FHDR - fctrl */
+    m.fhdr_fctrl  = p->payload[5];
+
+    /* FHDR - fcnt */
+    m.fhdr_fcnt   = p->payload[6];
+    m.fhdr_fcnt  |= p->payload[7] << 8;
 }
 
 void join_req_data(struct lgw_pkt_rx_s* p, struct molid_log_s* m) {
+    /* JRQ - Eui */
+    m.join_eui  = p->payload[8];
+    m.join_eui  = p->payload[9] << 8;
+    m.join_eui  = p->payload[10] << 16;
+    m.join_eui  = p->payload[11] << 24;
+
+    /* JRQ - DevEui */
+    m.join_deveui  = p->payload[4];
+    m.join_deveui  = p->payload[5] << 8;
+    m.join_deveui  = p->payload[6] << 16;
+    m.join_deveui  = p->payload[7] << 24;
+
     /* JRQ - DevNonce */
     m.join_devnonce  = p->payload[17];
     m.join_devnonce |= p->payload[18] << 8;
+}
+
+void join_acc_data(struct lgw_pkt_rx_s* p, struct molid_log_s* m) {
 }
 
 void molid_log(struct lgw_pkt_rx_s* p) {
